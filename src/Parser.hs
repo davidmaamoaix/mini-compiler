5fd5deef4b3c3760ccd65b094bda54b9f6013071
BuildTools <davidmaamoaix@gmail.com>
2021-06-01 20:29:12 +0800

    combinators

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,30 +1,16 @@
 module Parser where
 
+import Control.Monad
 import Text.ParserCombinators.Parsec
 
 file :: GenParser Char st [[String]]
-file = do
-    result <- many line
-    eof
-    return result
+file = line `endBy` char '\n'
 
 line :: GenParser Char st [String]
-line = do
-    result <- cells
-    char '\n'
-    return result
-
-cells :: GenParser Char st [String]
-cells = do
-    first <- cell
-    remain <- cellRemain
-    return $ first : remain
+line = cell `sepBy` char ','
 
 cell :: GenParser Char st String
 cell = many $ noneOf ",\n"
-
-cellRemain :: GenParser Char st [String]
-cellRemain = (char ',' >> cells) <|> return []
 
 parseFile :: String -> Either ParseError [[String]]
 parseFile = parse file "wat"
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,4 +2,4 @@ import Parser
 
 main :: IO ()
 main = do
-    putStrLn $ show $ parseFile "a, b, c\n"
+    putStrLn $ show $ parseFile "a, b, c"
