d525161f3c7cbc01cfaf8732ce275acf815b5c73
BuildTools <davidmaamoaix@gmail.com>
2021-06-01 20:38:57 +0800

    eol

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,16 +1,18 @@
 module Parser where
 
-import Control.Monad
 import Text.ParserCombinators.Parsec
 
 file :: GenParser Char st [[String]]
-file = line `endBy` char '\n'
+file = line `endBy` eol
 
 line :: GenParser Char st [String]
 line = cell `sepBy` char ','
 
 cell :: GenParser Char st String
 cell = many $ noneOf ",\n"
+
+eol :: GenParser Char st String
+eol = try (string "\n\r") <|> string "\n"
 
 parseFile :: String -> Either ParseError [[String]]
 parseFile = parse file "wat"
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,4 +2,4 @@ import Parser
 
 main :: IO ()
 main = do
-    putStrLn $ show $ parseFile "a, b, c"
+    putStrLn $ show $ parseFile "a, b, c\n\r"
