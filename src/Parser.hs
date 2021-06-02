e26979622ad2505d7fd39f5086e1ff9fa2561621
BuildTools <davidmaamoaix@gmail.com>
2021-06-02 16:04:54 +0800

    json finished

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,7 +1,7 @@
 module Parser where
 
 import Control.Monad
-import Control.Applicative
+import Control.Applicative hiding (many, (<|>))
 import Text.ParserCombinators.Parsec
 
 data JValue = JArray [JValue]
@@ -11,19 +11,36 @@ data JValue = JArray [JValue]
             deriving Show
 
 parseFile :: Parser JValue
-parseFile = spaces *> content <?> "Json Object Failed"
+parseFile = spaces *> content <?> "Json File Failed"
     where
-        content = parseArray -- <|> parseObject
+        content = parseArray <|> parseObject
 
 parseSeq :: Char -> Parser a -> Char -> Parser [a]
 parseSeq l p r = between (char l <* spaces) (char r) $
                  (p <* spaces) `sepBy` (char ',' <* spaces)
 
+parseAny :: Parser JValue
+parseAny = parseArray
+       <|> parseNumber
+       <|> parseString
+       <|> parseObject
+
 parseArray :: Parser JValue
-parseArray = JArray <$> parseSeq '[' parseNumber ']'
+parseArray = JArray <$> parseSeq '[' parseAny ']'
 
 parseNumber :: Parser JValue
 parseNumber = (JNumber . read) <$> many1 digit
 
+parseStringRaw :: Parser String
+parseStringRaw = between (char '"') (char '"') (many $ noneOf "\"")
+
+parseString :: Parser JValue
+parseString = JString <$> parseStringRaw
+
+parseObject :: Parser JValue
+parseObject = JObject <$> parseSeq '{' parseEntry '}'
+    where
+        parseEntry = (,) <$> (parseStringRaw <* spaces <* char ':' <* spaces) <*> parseAny
+
 testParser :: String -> Either ParseError JValue
-testParser = parse parseFile "wa"
+testParser = parse parseFile "(stdio)"
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,4 +2,4 @@ import Parser
 
 main :: IO ()
 main = do
-    putStrLn $ show $ testParser " [  1  , 2  , 3  ]   "
+    putStrLn $ show $ testParser "  { \" abc \" : \"hello\" , \"def\" : 123 }  "
