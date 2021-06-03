b2cb67ed35885b0e1c30956e96de21c75a0c3b4a
BuildTools <davidmaamoaix@gmail.com>
2021-06-03 18:37:09 +0800

    reserved stuff

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,1 +1,39 @@
 module Lexer where
+
+import Text.Parsec.String
+import Text.Parsec.Language
+
+import qualified Text.Parsec.Token as Token
+
+lexer :: Token.TokenParser ()
+lexer = Token.makeTokenParser emptyDef {
+    Token.commentLine = "//",
+    Token.commentStart = "/*",
+    Token.commentEnd = "*/",
+    Token.caseSensitive = True,
+    Token.reservedNames = [
+        "true",
+        "false",
+        "NULL",
+        "alloc",
+        "alloc_array",
+        "return",
+        "break",
+        "continue",
+        "if",
+        "while",
+        "for",
+        "struct",
+        "void",
+        "bool",
+        "int",
+        "typedef"
+    ],
+    Token.reservedOpNames = [
+        "+", "-", "*", "/", "%", "&", "|", "^",
+        "&&", "||", "<<", ">>", "<", ">", "<=",
+        ">=", "==", "!=", "!", "~", "-=", "+=",
+        "*=", "/=", "%=", "&=", "|=", "^=", "<<=",
+        ">>=", "=", "++", "--"
+    ]
+}
