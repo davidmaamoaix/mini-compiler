eb219f54367d63cec9c7c9008400b629d866b444
BuildTools <davidmaamoaix@gmail.com>
2021-09-06 08:27:29 +0800

    parser gadt setup

--- src/Parser.hs
+++ src/Parser.hs
@@ -12,5 +12,18 @@ import Text.ParserCombinators.Parsec
 
 type Parser a = Parsec [TokenPos] () a
 
+data Assign
+data Exp
+data GlobDecl
+data VarDecl = VarDecl { varType :: String
+                       , varId :: String
+                       }
+
 data Syntax a where
     Lit :: ValueType -> Syntax ValueType
+    FuncCall :: String -> [Syntax Exp] -> Syntax Exp
+    DeclStruct :: String -> Syntax GlobDecl
+    DefStruct :: String -> [VarDecl] -> Syntax GlobDecl
+    DeclVar :: [VarDecl] -> Syntax VarDecl
+    GlobVar :: Syntax VarDecl -> Syntax GlobDecl
+    TypeDef :: String -> String -> Syntax GlobDecl
