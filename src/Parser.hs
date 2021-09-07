0a5e7078eb961b32a68ab70f1e0a4879f86ace0d
BuildTools <davidmaamoaix@gmail.com>
2021-09-07 13:35:51 +0800

    gadt stuff

--- src/Parser.hs
+++ src/Parser.hs
@@ -15,15 +15,41 @@ type Parser a = Parsec [TokenPos] () a
 data Assign
 data Exp
 data GlobDecl
-data VarDecl = VarDecl { varType :: String
+data Stmt
+data VarType
+data LValue
+data ValDecl = ValDecl { varType :: VarType
                        , varId :: String
                        }
 
 data Syntax a where
+
+    -- basic constructs
+    DeclStruct :: String -> Syntax GlobDecl
+    DefStruct :: String -> [ValDecl] -> Syntax GlobDecl
+    GlobVar :: [ValDecl] -> Syntax GlobDecl
+    TypeDef :: VarType -> String -> Syntax GlobDecl
+    DeclVar :: ValDecl -> Syntax Exp -> Syntax ValDecl
+    FuncDef :: ValDecl -> [ValDecl] -> Syntax GlobDecl
+    IdType :: String -> Syntax VarType
+    PtrType :: Syntax VarType -> Syntax VarType
+    ArrType :: Syntax VarType -> Syntax VarType
+    StcType :: String -> Syntax VarType
+
+    -- left values
+    IdVar :: String -> Syntax LValue
+    StcMember :: Syntax LValue -> String -> Syntax LValue
+    PtrMember :: Syntax LValue -> String -> Syntax LValue
+    DerefVar :: Syntax LValue -> Syntax LValue
+    AsnArr :: Syntax LValue -> Syntax Exp -> Syntax LValue
+
+    -- expressions
     Lit :: ValueType -> Syntax ValueType
     FuncCall :: String -> [Syntax Exp] -> Syntax Exp
-    DeclStruct :: String -> Syntax GlobDecl
-    DefStruct :: String -> [VarDecl] -> Syntax GlobDecl
-    GlobVar :: [VarDecl] -> Syntax GlobDecl
-    TypeDef :: String -> String -> Syntax GlobDecl
-    DeclVar :: VarDecl -> Syntax Exp -> Syntax VarDecl
+
+    -- statements
+    StmtBlock :: [Stmt] -> Syntax [Stmt]
+    IncStmt :: String -> Syntax Stmt
+    DecStmt :: String -> Syntax Stmt
+    ExpStmt :: Syntax Exp -> Syntax Stmt
+    AsnStmt :: Syntax LValue -> String -> Exp -> Syntax Stmt
