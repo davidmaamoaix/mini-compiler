5cdf91b22c327c9ec548e244218373d116a9f1db
BuildTools <davidmaamoaix@gmail.com>
2022-01-01 19:58:50 +0800

    hmm try passing down dest?

--- src/StaticCheck.hs
+++ src/StaticCheck.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 module StaticCheck where
 
+import Control.Monad.State.Lazy
+import qualified Data.Map as M
+
 import Parser
 
-type Var = String
+type Var = Int
 
 -- Corresponds to a binary operation to use in IR.
 -- TODO: implment conversion to x86 translation.
@@ -17,12 +23,23 @@ data BinFunc
 
 data SSA
     = SMove { dest :: Var, src :: Var }
-    | SBinFunc { dest :: Var, op1, op2 :: Var, func :: BinFunc }
+    | SBinFunc { dest :: Var, func :: BinFunc, op1, op2 :: Var }
+
+data Env = Env
+    { varCount :: Int
+    , varRef :: M.Map String Int
+    , code :: [SSA]
+    }
+
+-- Typeclass that matches GADT nodes based on whether
+-- its SSA form has a return register.
+class ConvertSSA a o | a -> o where
+    conv :: State Env o
 
 staticCheck :: Node Prog -> Maybe (Int, String)
 staticCheck (NProg xs) = Nothing
 
--- Converts a function to Static Single Assignment form.
--- TODO: change to function in future labs.
+-- Converts code to Static Single Assignment form.
+-- TODO: change to target a function in future labs.
 toSSA :: Node Prog -> [SSA]
-toSSA = undefined
+toSSA x = undefined
